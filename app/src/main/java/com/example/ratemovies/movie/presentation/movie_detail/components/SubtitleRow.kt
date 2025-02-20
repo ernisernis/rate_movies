9322afb09934e5cb3202ad9b11a1aae5cf752912
ernisernis <ernestas.dainys@gmail.com>
2025-02-20 17:13:11 +0200

    Remove PreviewDark preview from SubtitleRow

--- app/src/main/java/com/example/ratemovies/movie/presentation/movie_detail/components/SubtitleRow.kt
+++ app/src/main/java/com/example/ratemovies/movie/presentation/movie_detail/components/SubtitleRow.kt
@@ -7,7 +7,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.ratemovies.movie.presentation.models.toDisplayableRuntime
 import com.example.ratemovies.movie.presentation.movie_detail.defaultMovieDetail
@@ -37,7 +37,7 @@ fun SubtitleRow(
     }
 }
 
-@PreviewLightDark
+@Preview
 @Composable
 fun SubtitleRowPreview() {
     RateMoviesTheme {
