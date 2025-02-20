ab605fe873e270b318ab82df4ec022636e6115d2
ernisernis <ernestas.dainys@gmail.com>
2025-02-20 17:29:09 +0200

    Create icon objects

---
+++ app/src/main/java/com/example/ratemovies/core/presentation/util/RmIcons.kt
@@ -0,0 +1,28 @@
+package com.example.ratemovies.core.presentation.util
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.ArrowBackIosNew
+import androidx.compose.material.icons.filled.Bookmark
+import androidx.compose.material.icons.filled.BookmarkBorder
+import androidx.compose.material.icons.filled.Bookmarks
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.filled.StarOutline
+import androidx.compose.material.icons.filled.StarRate
+import androidx.compose.material.icons.filled.ThumbUp
+
+object RmIcons {
+    val Back = Icons.Default.ArrowBackIosNew
+    val Star = Icons.Default.Star
+    val StarRate = Icons.Default.StarRate
+    val StarOutline = Icons.Default.StarOutline
+    val ThumbUp = Icons.Default.ThumbUp
+    val Home = Icons.Default.Home
+    val Bookmarks = Icons.Default.Bookmarks
+    val BookmarkUnselected = Icons.Default.BookmarkBorder
+    val BookmarkSelected = Icons.Default.Bookmark
+    val Profile = Icons.Default.AccountCircle
+    val MoreVert = Icons.Default.MoreVert
+}
