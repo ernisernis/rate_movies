194462d7cc465b09e048c79dbc58624998637aeb
ernisernis <ernestas.dainys@gmail.com>
2025-02-20 16:40:10 +0200

    Add movie detail screen dimen paddings

--- app/src/main/java/com/example/ratemovies/core/presentation/util/Dimens.kt
+++ app/src/main/java/com/example/ratemovies/core/presentation/util/Dimens.kt
@@ -30,17 +30,27 @@ object Dimens {
         @Composable get() = 16.dp
 
 
-    /*
-        movie_details
-     */
-    val MovieDetailsAlpha: Float
-        get() = 0.8f
-
+    // movie_detail
     val MovieDetailContainerPadding: Dp
         @Composable get() = 16.dp
 
+    val MovieDetailItemPaddingBig: Dp
+        @Composable get() = 16.dp
+
+    val MovieDetailItemPaddingNormal: Dp
+        @Composable get() = 8.dp
+
+    val MovieDetailItemPaddingSmall: Dp
+        @Composable get() = 4.dp
+
     val MovieDetailComponentPadding: PaddingValues
-        @Composable get() = PaddingValues(horizontal = MovieDetailContainerPadding, vertical = MovieDetailContainerPadding / 2)
+        @Composable get() = PaddingValues(
+            horizontal = MovieDetailItemPaddingBig,
+            vertical = MovieDetailItemPaddingNormal
+        )
+
+    val MovieDetailAlpha: Float
+        get() = 0.8f
 
     /*
         movie_rate
