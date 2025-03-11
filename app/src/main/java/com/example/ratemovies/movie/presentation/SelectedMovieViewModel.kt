b93b46d5699784474f6982a87d3510b846d397d9
ernisernis <ernestas.dainys@gmail.com>
2025-03-11 16:22:16 +0200

    Fix movie being empty when clicked from bookmark or profile tab

--- app/src/main/java/com/example/ratemovies/movie/presentation/SelectedMovieViewModel.kt
+++ app/src/main/java/com/example/ratemovies/movie/presentation/SelectedMovieViewModel.kt
@@ -1,15 +1,21 @@
 package com.example.ratemovies.movie.presentation
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ratemovies.core.domain.util.onError
+import com.example.ratemovies.core.domain.util.onSuccess
 import com.example.ratemovies.movie.domain.model.Movie
+import com.example.ratemovies.movie.domain.repository.MovieRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 // Shared ViewModel scoped to Route.MovieGraph
 @HiltViewModel
 class SelectedMovieViewModel @Inject constructor(
+    private val movieRepository: MovieRepository,
 ): ViewModel() {
 
     private val _selectedMovie = MutableStateFlow<Movie?>(null)
@@ -20,6 +26,16 @@ class SelectedMovieViewModel @Inject constructor(
     }
 
     fun onSelectMovieId(id: Int) {
-        // TODO:
+        viewModelScope.launch {
+            movieRepository
+                .getMovie(id)
+                .onSuccess { movie ->
+                    _selectedMovie.value = movie
+                }
+                .onError {
+                    // TODO implement
+                }
+        }
     }
+
 }
